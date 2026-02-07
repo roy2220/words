019b4fab9ea6d4d5adee8b6757e1427353f5ecc9
Roy O'Young <roy2220@outlook.com>
2026-02-07 11:57:24 +0800

    Parallel process

--- extract-words.bash
+++ extract-words.bash
@@ -1,8 +1,16 @@
 set -euxo pipefail
 
+nproc
+
+JQ_CMD=(
+	jq
+	--raw-output
+	'select(.lang == "English" and (.word | test("^[a-zA-Z]+$"))) | .word | ascii_downcase'
+)
+
 curl -SsLf https://kaikki.org/dictionary/raw-wiktextract-data.jsonl.gz |
-	gzip --decompress --stdout |
-	jq --raw-output 'select(.lang == "English" and (.word | test("^[a-zA-Z]+$"))) | .word | ascii_downcase' |
+	gzip --decompress |
+	parallel --pipe --block=8M --round-robin --line-buffer --will-cite "${JQ_CMD[@]@Q}" |
 	pv --line-mode --format='%b' --numeric --interval=5 --force >words.txt
 
 sort --unique --output words.txt words.txt
